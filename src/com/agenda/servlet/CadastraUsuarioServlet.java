025728e15375d926f255de287b560e3125997194
Carlosfloresta <carlos.floresta@etec.sp.gov.br>
2019-05-23 22:04:58 -0300

    Atualizado23/05

--- src/com/agenda/servlet/CadastraUsuarioServlet.java
+++ src/com/agenda/servlet/CadastraUsuarioServlet.java
@@ -3,7 +3,6 @@ package com.agenda.servlet;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,16 +10,11 @@ import javax.servlet.http.HttpServletResponse;
 import com.agenda.model.Pessoa;
 import com.agenda.service.CadastraUsuarioService;
 
-
-
-
 public class CadastraUsuarioServlet extends HttpServlet {
 	
-	
-	
-	
+	private static final long serialVersionUID = -3439992183507416139L;
+
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	
 		
 		request.setCharacterEncoding("UTF-8");
 		Pessoa pessoa = new Pessoa();
@@ -29,29 +23,15 @@ public class CadastraUsuarioServlet extends HttpServlet {
 		pessoa.setEmail(request.getParameter("email"));
 		pessoa.setEnd(request.getParameter("end"));
 		CadastraUsuarioService service = new CadastraUsuarioService();
-		
-			
 			
 		if(null != request.getParameter("id") && !request.getParameter("id").equals("")) {
-			
+			pessoa.setId(Integer.parseInt(request.getParameter("id")));
 			service.alterarContato(pessoa);
 		}else {
 			service.cadastra(pessoa);
 		}
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+
 		response.sendRedirect("busca-contato");
-		
 		
 	}
 }
